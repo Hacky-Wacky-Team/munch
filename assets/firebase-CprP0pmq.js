7b5c0cb641a2232029ac628ef0c04cbc3a2e41ed
Jonathan Zhou <jonnyzgg@gmail.com>
2026-03-01 05:44:04 -0500

    Updates

---
+++ assets/firebase-CprP0pmq.js
@@ -0,0 +1,16 @@
+import{r as e,g as a,i as r}from"./index.esm-pTxCuGF7.js";var i="firebase",s="12.1.0";/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */e(i,s,"app");const p={apiKey:"AIzaSyAgF0T9likRBmNMNjkLuCR2prMUtirpxF4",authDomain:"munch-6303f.firebaseapp.com",projectId:"munch-6303f",storageBucket:"munch-6303f.firebasestorage.app",messagingSenderId:"665224414010",appId:"1:665224414010:web:707a2c598cffdfec238603",measurementId:"G-E1FED4ZF44"},t=r(p),o=a(t);export{o as db};
