63a12594aea190dcdc1af81d3fac403e79b06eaa
Jonathan Zhou <jonnyzgg@gmail.com>
2026-05-20 06:59:42 -0400

    Updates

---
+++ assets/firebase-CSfcG6hO.js
@@ -0,0 +1,16 @@
+import{r,i as o}from"./index.esm-C6IHFrXA.js";import{getFirestore as a}from"./index.esm-DfFIrC7I.js";import{getAuth as i,GoogleAuthProvider as t}from"./index.esm-rjx95Lop.js";var p="firebase",s="12.1.0";/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */r(p,s,"app");const n={apiKey:"AIzaSyAgF0T9likRBmNMNjkLuCR2prMUtirpxF4",authDomain:"munch-6303f.firebaseapp.com",projectId:"munch-6303f",storageBucket:"munch-6303f.firebasestorage.app",messagingSenderId:"665224414010",appId:"1:665224414010:web:707a2c598cffdfec238603",measurementId:"G-E1FED4ZF44"},e=o(n),g=a(e),u=i(e),d=new t;export{u as auth,g as db,d as googleProvider};
