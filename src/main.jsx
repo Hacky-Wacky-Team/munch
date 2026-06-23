2f231620d722cb4689c1219dcc4201eb39502822
Jonathan Zhou <jonnyzgg@gmail.com>
2026-06-22 21:07:15 -0400

    7.6 vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 import PostLanding from './components/PostLanding.jsx'
@@ -18,6 +19,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/tos" element={<TermsOfService />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
